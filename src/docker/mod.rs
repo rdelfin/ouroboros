b20ba091b21f88f8313b72e768c67a1112d75abe
Ricardo Delfin <me@rdelfin.com>
2020-11-08 21:27:39 +0000

    Moved API call out into separate function

--- src/docker/mod.rs
+++ src/docker/mod.rs
@@ -1,8 +1,10 @@
 mod structs;
 
 use self::structs::{Container, DockerErrorBody};
+use bytes::buf::BufExt;
 use hyper::{Client, Error as HyperError};
 use hyperlocal::{UnixClientExt, Uri};
+use serde::de;
 use serde_json::Error as JsonError;
 use std::path::Path;
 use thiserror::Error;
@@ -18,20 +20,24 @@ pub enum DockerError {
 }
 
 pub async fn list() -> Result<Vec<Container>, DockerError> {
+    call_api::<Vec<Container>>("/v1.40/containers/json").await
+}
+
+async fn call_api<'a, T: de::DeserializeOwned>(url_path: &str) -> Result<T, DockerError> {
     let path = Path::new("/var/run/docker.sock");
-    let url = Uri::new(path, "/v1.40/containers/json").into();
+    let url = Uri::new(path, url_path).into();
 
     let client = Client::unix();
 
     let res = client.get(url).await?;
+    let status = res.status();
+    let body = hyper::body::aggregate(res).await?;
 
-    if res.status() == 200 {
-        let buf = hyper::body::to_bytes(res).await?;
-        Ok(serde_json::from_slice::<Vec<Container>>(&buf[..])?)
+    if status == 200 {
+        Ok(serde_json::from_reader(body.reader())?)
     } else {
-        let buf = hyper::body::to_bytes(res).await?;
-        Err(DockerError::APIError(serde_json::from_slice::<
-            DockerErrorBody,
-        >(&buf[..])?))
+        Err(DockerError::APIError(serde_json::from_reader(
+            body.reader(),
+        )?))
     }
 }
